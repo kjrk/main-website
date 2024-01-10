79ac4e9cd2bc9bffab3d2eb189f4e4861b477952
kjrk <60024620+kjrk@users.noreply.github.com>
2024-01-10 22:46:04 +1100

    Added basic home page social media icons

--- main-website/src/App.js
+++ main-website/src/App.js
@@ -1,22 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./css/social-media.css";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 5,
+            delay: 1,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
         >
-          Learn React
-        </a>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Welcome</p>
+          <div
+            style={{
+              display: "flex",
+              direction: "row",
+              gap: "20px",
+              justifyContent: "center",
+            }}
+          >
+            <motion.div
+              whileHover={{ scale: [null, 1.5, 1.4] }}
+              transition={{ duration: 0.3 }}
+            >
+              <a href="https://www.youtube.com/" className="youtube social">
+                <FontAwesomeIcon icon={faYoutube} size="2x" />
+              </a>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: [null, 1.5, 1.4] }}
+              transition={{ duration: 0.3 }}
+            >
+              <a href="https://www.facebook.com/" className="facebook social">
+                <FontAwesomeIcon icon={faFacebook} size="2x" />
+              </a>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: [null, 1.5, 1.4] }}
+              transition={{ duration: 0.3 }}
+            >
+              <a href="https://www.twitter.com/" className="twitter social">
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+              </a>
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: [null, 1.5, 1.4] }}
+              transition={{ duration: 0.3 }}
+            >
+              <a href="https://www.instagram.com/" className="instagram social">
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              </a>
+            </motion.div>
+          </div>
+        </motion.div>
       </header>
     </div>
   );
